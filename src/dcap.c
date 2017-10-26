dCache/dcap

commit 96e85d4e02784a8f6e219385f3e9d339ad21d316

dcap_url: accept IPv6 formated urls
split host to host and port

Target: master
Acked-By: Albert Rossi

src/dcap.c
@@ -122,6 +122,7 @@ static void getPortRange();
 static int isActive();
 static ConfirmationBlock get_reply(int);
 static int name_invalid(char *);
+static server * url2srv(dcap_url *);
 
 #ifndef HAVE_HTONLL
     uint64_t  htonll(uint64_t);
@@ -542,7 +543,7 @@ serverConnect(struct vsp_node * node)
 		lockMember();
 		if ((node->fd = getMember(dcache_host)) != -1) {
 
-			srv = parseConfig(node->url == NULL ? dcache_host : url2config(buffer, sizeof(buffer), node->url) );
+			srv = node->url == NULL ? parseConfig(dcache_host) : url2srv(node->url);
 			if (srv == NULL) {
 				unlockMember();
 				return -1;
@@ -560,7 +561,7 @@ serverConnect(struct vsp_node * node)
 
 		newQueue(0);
 		dc_debug(DC_INFO, "Creating a new control connection to %s.",dcache_host );
-		srv = parseConfig(node->url == NULL ? dcache_host : url2config(buffer, sizeof(buffer), node->url) );
+		srv = node->url == NULL ? parseConfig(dcache_host) : url2srv(node->url);
 
 		if (srv == NULL) {
 			unlockMember();
@@ -1993,4 +1994,31 @@ static int isActive()
 void dc_setClientActive()
 {
 	activeClient=1;
+}
+
+server * url2srv(dcap_url *url)
+{
+
+    server *srv;
+    char *tunnel;
+
+    srv = (server *) malloc(sizeof (server));
+    if (srv == NULL) {
+        dc_errno = DESYS;
+        return NULL;
+    }
+
+    srv->hostname = strdup(url->host);
+    srv->port = url->port;
+
+    if (url->prefix != NULL) {
+        tunnel = malloc(64);
+        tunnel[0] = '\0';
+        sprintf(tunnel, "lib%sTunnel.so", url->prefix);
+        srv->tunnel = addIoPlugin(tunnel);
+        free(tunnel);
+    } else {
+        srv->tunnel = NULL;
+    }
+    return srv;
 }

src/dcap_url.c
@@ -28,6 +28,7 @@
 #include "debug_level.h"
 #include "xutil.h"
 #include "dcap_str_util.h"
+#include "tunnelManager.h"
 
 #define DCAP_PREFIX "dcap://"
 #define PNFS_PREFIX "pnfs://"
@@ -48,13 +49,13 @@ dcap_url* dc_getURL( const char *path )
 	dcap_url *url;
 	char *s;
 	char *w;
-	char *host;
+	char *port_str;
 	int host_len;
 	int type = URL_NONE;
 	int def_door_len;
 	char *domain;
 	struct servent *se;
-	short port;
+        char *t;
 
 
 	if(path == NULL) {
@@ -92,6 +93,8 @@ dcap_url* dc_getURL( const char *path )
 	url->file = NULL;
 	url->prefix = NULL;
 	url->type= type;
+        url->need_brackets = 0;
+        url->port = -1;
 
 
 	if( s != path ) {
@@ -118,29 +121,44 @@ dcap_url* dc_getURL( const char *path )
 
     if( host_len != 0 ) {
 
-		host = xstrndup(s, host_len );
+                if (s[0] == '[') {
+                    /* ipv6 address */
+                    s++;
+                    t = strchr(s, ']');
+                    if (t == NULL || t > w) {
+                        /* bad formated url */
+                        dc_errno = DEURL;
+                        dc_debug(DC_ERROR, "badly formed url for %s", path);
+                        free(url);
+                        return NULL;
+                    }
+                    url->need_brackets = 1;
+                    url->host = xstrndup(s, t - s);
+                    s = t+1;
+                } else {
+                    t = strchr(s, ':');
+                    if (t == NULL || t > w) {
+                        url->host = xstrndup(s, w - s);
+                        s = w;
+                    } else {
+                        url->host = xstrndup(s, t - s);
+                        s = t;
+                    }
+                }
 
-		if(host == NULL) {
-			dc_debug(DC_ERROR, "Failed to duplicate host in url %s", path);
-			free(url);
-			return NULL;
-		}
+                /* s points to ':' or '/' */
 
-
-		/* if port not specified, take it from /etc/services or fall back to default */
-		w = strchr(host, ':');
-		if( w == NULL ) {
+		if( s[0] == '/' ) {
 			w = strchr(path, ':');
 			w = xstrndup(path, w - path);
 			se = getservbyname(w, "tcp");
 			free(w);
-			port = se ? ntohs(se->s_port) : DEFAULT_DOOR_PORT;
-			url->host = malloc(host_len + 1 + 8);
-			url->host[0] = '\0';
-			sprintf(url->host, "%s:%d", host, port );
-			free(host);
+			url->port = se ? ntohs(se->s_port) : DEFAULT_DOOR_PORT;
 		}else{
-			url->host = host;
+                        s++;
+                        port_str = xstrndup(s, w-s);
+                        url->port = atoi(port_str);
+                        free(port_str);
 		}
 
 
@@ -184,17 +202,3 @@ dcap_url* dc_getURL( const char *path )
 
 	return url;
 }
-
-char * url2config(char *configLine, size_t configLineSize, dcap_url *url)
-{
-
-	snprintf(configLine, configLineSize, "%s", url->host);
-	configLine[configLineSize-1] = '\0';
-
-	if ( url->prefix != NULL ) {
-		dc_snaprintf(configLine, configLineSize, ":lib%sTunnel.so", url->prefix );
-	}
-
-	return configLine;
-}
-

src/dcap_url.h
@@ -26,10 +26,11 @@ typedef struct {
 	char *file;
 	int type;
 	char *prefix;
+        short port;
+        int need_brackets;
 } dcap_url;
 
 extern dcap_url *dc_getURL(const char *);
-extern char *url2config( char *, size_t, dcap_url *);
 extern int isUrl(const char *);
 
 #endif /* DCAP_URL_H */

commit 261dcecea1920b438534a3e289969ffdd40429e0

libdcap: fix DCACHE_IO_TUNNEL env handling (regression in 96e85d4)
Motivation:
changes in 96e85d4 introduced regression that lead to ignoring
evn DCACHE_IO_TUNNEL is url syntax is used.

Modification:
take into account DCACHE_IO_TUNNEL env when url is used.

Result:
dcap with URL respects DCACHE_IO_TUNNEL env

Acked-by: Paul Millar
Target: master

src/dcap.c
@@ -2041,9 +2041,11 @@ server * url2srv(dcap_url *url)
     srv->hostname = strdup(url->host);
     srv->port = url->port;
 
-    if (url->prefix != NULL) {
-        tunnel = malloc(64);
-        tunnel[0] = '\0';
+    tunnel = getenv("DCACHE_IO_TUNNEL");
+    if (tunnel != NULL) {
+        srv->tunnel = addIoPlugin(tunnel);
+    } else if (url->prefix != NULL) {
+        tunnel = malloc(strlen(url->prefix) + 12 + 1); // len(prefix) + len(libTunnel.so) + '\0'
         sprintf(tunnel, "lib%sTunnel.so", url->prefix);
         srv->tunnel = addIoPlugin(tunnel);
         free(tunnel);